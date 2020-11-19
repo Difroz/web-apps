be75fd405669d1733d566cfb4c166c795ff0ee6a
admin <admin@adminss.com>
2020-11-19 20:43:47 +0700

    send mail in django

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,6 +1,16 @@
+from django.views.generic import ListView
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
+from .forms import EmailPostFrom
+from django.core.mail import send_mail
+
+
+class PostListView(ListView):
+    queryset = Post.objects.filter(status='published')
+    paginate_by = 3
+    context_object_name = 'posts'
+    template_name = 'blog/post/list.html'
 
 
 def post_list(request):
@@ -29,3 +39,25 @@ def post_detail(request, year, month, day, post):
                              publish__month=month,
                              publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+    if request.method == 'POST':
+        form = EmailPostFrom(request.POST)
+
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = '{} ({}) recommends you reading "{}"'.format(cd['name'], cd['email'], post.title)
+            message = 'Read "{}" at {}\n\n{}\'s comments: {}'.format(post.title, post_url, cd['name'], cd['comments'])
+            send_mail(subject, message, 'admin@myblog.com', [cd['to']])
+            sent = True
+    else:
+        form = EmailPostFrom()
+    return render(request, 'blog/post/share.html', {
+        'post': post,
+        'form': form,
+        'sent': sent,
+    })
