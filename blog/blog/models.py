126c55c2bb33b21cf1d6d91de662c9741b63c193
proger <proger@company.com>
2020-11-21 15:59:41 +0700

    created: tags tags filter tags for django {{% %}}

--- blog/blog/models.py
+++ blog/blog/models.py
@@ -3,6 +3,8 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 
+from taggit.managers import TaggableManager
+
 
 class Post(models.Model):
     STATUS_CHOICES = (
@@ -17,6 +19,7 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    tags = TaggableManager()
 
     class Meta:
         ordering = ('-publish',)
